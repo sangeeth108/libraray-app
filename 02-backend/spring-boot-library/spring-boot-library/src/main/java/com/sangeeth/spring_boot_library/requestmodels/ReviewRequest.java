20d2d0aece096d1ee5d61c6fb231f0e6115bd7bb
sangeeth108 <thisaranga108@gmail.com>
2025-04-07 07:30:16 +0530

    name change

---
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/sangeeth/spring_boot_library/requestmodels/ReviewRequest.java
@@ -0,0 +1,18 @@
+package com.sangeeth.spring_boot_library.requestmodels;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Optional;
+
+@Getter
+@Setter
+public class ReviewRequest {
+
+    private double rating;
+
+    private Long bookId;
+
+    private Optional<String> reviewDescription;
+
+}
